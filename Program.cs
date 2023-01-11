1a78ac4ab1fe826a252e728f12f75e740f9bc9ed
unknown <LocalHost@PC2022-07.ad.pslib.cz>
2023-01-11 11:09:19 +0100

    awjsdajkgG

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using morseovka;
+
 Console.WriteLine("Hello, World!");
+rumicek omegalul = new rumicek();
+
+string encryptedString = omegalul.Encode("jak se máš?");
+Console.WriteLine(encryptedString);
+Console.WriteLine(omegalul.Decode(encryptedString));
---
+++ rumicek.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace morseovka
+{
+    internal class rumicek
+    {
+        private Dictionary<string, string> _morseCodeDictionary = new Dictionary<string, string>
+        {
+            {"A", ".-"}, {"B", "-..."}, {"C", "-.-."}, {"D", "-.."}, {"E", "."},
+            {"F", "..-."}, {"G", "--."}, {"H", "...."}, {"CH", "----"}, {"I", ".."}, {"J", ".---"},
+            {"K", "-.-"}, {"L", ".-.."}, {"M", "--"}, {"N", "-."}, {"O", "---"},
+            {"P", ".--."}, {"Q", "--.-"}, {"R", ".-."}, {"S", "..."}, {"T", "-"},
+            {"U", "..-"}, {"V", "...-"}, {"W", ".--"}, {"X", "-..-"}, {"Y", "-.--"},
+            {"Z", "--.."},
+            {"0", "-----"}, {"1", ".----"}, {"2", "..---"}, {"3", "...--"},
+            {"4", "....-"}, {"5", "....."}, {"6", "-...."}, {"7", "--..."},
+            {"8", "---.."}, {"9", "----."},
+            {".", ".-.-.-"}, {",", "--..--"}, {"?", "..--.."}, {"!", "-.-.--"},
+            {"/", "-..-."}, {"(", "-.--."}, {")", "-.--.-"}, {"&", ".-..."},
+            {":", "---..."}, {";", "-.-.-."}, {"=", "-...-"}, {"+", ".-.-."},
+            {"-", "-....-"}, {"_", "..--.-"}, {"$", "...-..-"},
+            {"@", ".--.-."}, {" ", " "}, {"", ""}
+        };
+
+        private Dictionary<string, string> reversedDictionary => _morseCodeDictionary.ToDictionary(x => x.Value, x => x.Key);
+
+        static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+
+            for (int i = 0; i < normalizedString.Length; i++)
+            {
+                char c = normalizedString[i];
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder
+                .ToString()
+                .Normalize(NormalizationForm.FormC);
+        }
+        public string Encode(string text)
+        {
+            string oktext = RemoveDiacritics(text);
+            string result = "";
+            foreach (var i in oktext)
+            {
+                result += _morseCodeDictionary[i.ToString().ToUpper()] + "/";
+            }
+            return result;
+        }
+
+        public string Decode(string text)
+        {
+;           string result = "";
+
+            string[] oktext = text.Split("/");
+
+            foreach (var i in oktext)
+            {
+                result += reversedDictionary[i];
+            }
+
+            return result;
+        }
+    }
+}
